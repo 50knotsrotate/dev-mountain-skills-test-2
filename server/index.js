e18b8b83e3402cb8b0584a781d7f0944d10947eb
= <=>
2019-04-05 10:10:28 -0700

    set up SQL databse

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const massive = require('massive')
 const controller = require('./controller')
+
+massive(process.env.CONNECTION_URI)
+    .then(db => { 
+        app.set('db', db)
+      // db.init()
+        console.log('connected to db')
+    }).catch(err => { 
+        console.log(err)
+    })
 
 const PORT = 4000;
 app.listen(PORT, () => { 
